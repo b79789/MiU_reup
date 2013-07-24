00c5d58f12da5f6cdbe9f8a26dedbfd2eae5c9d7
b79789 <bstacks78@fullsail.edu>
2013-07-24 10:22:52 -0400

    making final changes for week 3

--- Gold/js/main.js
+++ Gold/js/main.js
@@ -2,15 +2,12 @@ $("#home").on('pageinit', function(){
 	//code needed for home page goes here
 });
 
-var parseAddForm = function(data){
-	//use form data here
-	
-}
 
-$(document).on('pageinit', function(){
+$("#form").on('pageinit', function(){
 	
 	var valForm = $("#addForm"),
 	    myFormErrorLink = $("#addFormErrorLink");
+	
 		
 	valForm.validate({
 		invalidHandler: function(form, validator){
@@ -28,7 +25,32 @@ $(document).on('pageinit', function(){
 		submitHandler: function(){
 			var data = valForm.serializeArray();
 			parseAddForm(data);
+			alert("Thank You!");
 		}
+		
+		
 	});
+	var parseAddForm = function(data){
+	//use form data here
+	console.log(data);
+}
+
+function clearUserData(){
+		if(localStorage.length === 0){
+			alert("There is no data!")
+		}else{
+			localStorage.clear();
+			alert("All info wiped!");
+			window.location.reload();
+			return false;
+		}
+	}
 	
+function ge(x){
+		var myElement = document.getElementById(x);
+		return myElement;
+	}
+var clearData = ge("clearMy");
+	clearData.addEventListener("click",clearUserData);
 });
+
